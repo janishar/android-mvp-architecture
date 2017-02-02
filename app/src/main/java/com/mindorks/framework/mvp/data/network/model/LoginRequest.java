a8c0e4450d914f1ec50b03c8c7e6395fd3a1ac7c
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-02-02 22:03:15 +0530

    Add equals and hashCode

--- app/src/main/java/com/mindorks/framework/mvp/data/network/model/LoginRequest.java
+++ app/src/main/java/com/mindorks/framework/mvp/data/network/model/LoginRequest.java
@@ -57,6 +57,25 @@ public class LoginRequest {
         public void setPassword(String password) {
             this.password = password;
         }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+
+            ServerLoginRequest that = (ServerLoginRequest) object;
+
+            if (email != null ? !email.equals(that.email) : that.email != null) return false;
+            return password != null ? password.equals(that.password) : that.password == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = email != null ? email.hashCode() : 0;
+            result = 31 * result + (password != null ? password.hashCode() : 0);
+            return result;
+        }
     }
 
     public static class GoogleLoginRequest {
