7b47488d3ab714ea7c98aa7c0a1ea0ded91940e3
janishar <janishar.ali@gmail.com>
2017-02-12 23:08:57 +0530

    copy write url update

--- app/src/main/java/com/mindorks/framework/mvp/di/component/ActivityComponent.java
+++ app/src/main/java/com/mindorks/framework/mvp/di/component/ActivityComponent.java
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.mindorks.com/license/apache-v2
+ *     https://mindorks.com/license/apache-v2
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
--- app/src/main/java/com/mindorks/framework/mvp/service/SyncService.java
+++ app/src/main/java/com/mindorks/framework/mvp/service/SyncService.java
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.mindorks.com/license/apache-v2
+ *     https://mindorks.com/license/apache-v2
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,7 +26,6 @@ import com.mindorks.framework.mvp.MvpApp;
 import com.mindorks.framework.mvp.data.DataManager;
 import com.mindorks.framework.mvp.di.component.DaggerServiceComponent;
 import com.mindorks.framework.mvp.di.component.ServiceComponent;
-import com.mindorks.framework.mvp.di.module.ServiceModule;
 
 import javax.inject.Inject;
 
